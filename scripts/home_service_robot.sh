c8fd8d6af7cad539cfdf670bb354bb4e46d1b2bd
William Gillespie <william.gillespie@soartech.com>
2021-04-20 21:48:59 -0400

    Change launch to have marker visualization by default.

--- scripts/home_service_robot.sh
+++ scripts/home_service_robot.sh
@@ -12,7 +12,10 @@ sleep 10
 
 # TURTLEBOT_RVIZ_LAUNCHERS launches rviz and automatically loads the robot model, trajectories, and map.
 # package:  turtlebot_rviz_launchers , launch file: view_navigation.launch
-xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+# xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+# the marker_viz_rviz.launch is the same as the view_navigation.launch,
+# but automatically has marker visualization.
+xterm -e "roslaunch pick_objects marker_viz_rviz.launch" &
 sleep 5
 
 # PICK_OBJECTS c++ node.  This will direct the robot to the pickup location,
